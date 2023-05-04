0b3f1d192243bb373369e6909732cd7c3b85c8aa
Valdir Giorgi Notebook <valdirgiorgi@gmail.com>
2023-05-04 19:56:43 -0300

    commit inicial

---
+++ bin/dojo.dart
@@ -0,0 +1,5 @@
+import 'package:dojo/dojo.dart' as dojo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dojo.calculate()}!');
+}
---
+++ test/dojo_test.dart
@@ -0,0 +1,8 @@
+import 'package:dojo/dojo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
