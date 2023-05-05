f1c39ded002e4d4f023c4d23d8eae9104ae1b0e8
Valdir Giorgi Notebook <valdirgiorgi@gmail.com>
2023-05-04 21:42:47 -0300

    refatorando

--- lib/fizzbuzz.dart
+++ lib/fizzbuzz.dart
@@ -1,29 +1,15 @@
 class FizzBuzz {
   bool fizz(int numero) {
     num restoDivisao = numero % 3;
-    if (restoDivisao == 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return restoDivisao == 0 ? true : false;
   }
 
   bool buzz(int numero) {
     num restoDivisao = numero % 5;
-    if (restoDivisao == 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return restoDivisao == 0 ? true : false;
   }
 
   bool fizzBuzz(int numero) {
-    num restoDivisaoTres = numero % 3;
-    num restoDivisaoCinco = numero % 5;
-    if (restoDivisaoTres == 0 && restoDivisaoCinco == 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return fizz(numero) && buzz(numero) ? true : false;
   }
 }
