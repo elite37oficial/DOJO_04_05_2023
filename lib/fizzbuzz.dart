981ad232f72a5e8271476f1c0f6b1985b385f100
RaquelValgas <raquelvalgas@gmail.com>
2023-05-04 21:02:49 -0300

    inicio do exercicio FizzBuzz

---
+++ lib/fizzbuzz.dart
@@ -0,0 +1,14 @@
+class FizzBuzz {
+  divisor() {
+    List<int> numeros = [];
+    for (var i = 1; i < 101; i++) {
+      numeros.add(i);
+    }
+
+    for (var i = 0; i < 100; i++) {
+      if (numeros[i] % 3) {}
+    }
+
+    return "Teste";
+  }
+}
---
+++ test/fizzbuzz_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+
+import '../lib/fizzbuzz.dart';
+
+void main() {
+  FizzBuzz fizzBuzz = FizzBuzz();
+
+  test('fizzbuzz ...', () async {
+   expect(fizzBuzz.)
+  });
+}
