490852fcd5ea6d83d1f7d85cac6822c190c0f7a6
WellysonP <100440445+WellysonP@users.noreply.github.com>
2023-05-04 21:35:41 -0300

    adicionado for para criar lista

--- bin/dojo.dart
+++ bin/dojo.dart
@@ -1,5 +1,17 @@
 import 'package:dojo/dojo.dart' as dojo;
+import 'package:dojo/fizzbuzz.dart';
 
 void main() {
-  print('Hello world: ${dojo.calculate()}!');
+  FizzBuzz fizzBuzz = FizzBuzz();
+  for (int i = 0; i <= 100; i++) {
+    if (fizzBuzz.fizzBuzz(i) == true) {
+      print("FizzBuzz");
+    } else if (fizzBuzz.fizz(i) == true) {
+      print("Fizz");
+    } else if (fizzBuzz.buzz(i) == true) {
+      print("Buzz");
+    } else {
+      print(i);
+    }
+  }
 }
